cee3a021eb68446f7ede2050cdec290ff1e3e147
Esmira <esmirasar@yandex.ru>
2024-10-06 15:25:35 +0400

    flowers_table

--- database/flowers.py
+++ database/flowers.py
@@ -1,19 +1,17 @@
 import sqlite3
 
-connection = sqlite3.connect('tg_bot.db')
-cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS flowers(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    schedule INTEGER NOT NULL,
-    FOREIGN KEY (user_id)
-REFERENCES users(id)
-)
-''')
+def flowers_table(connection: sqlite3.Connection):
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS flowers(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        user_id INTEGER NOT NULL,
+        schedule INTEGER NOT NULL,
+        FOREIGN KEY (user_id)
+    REFERENCES users(id)
+    )
+    ''')
 
-connection.commit()
-connection.close()
-
+    connection.commit()
--- database/main.py
+++ database/main.py
@@ -1,9 +1,11 @@
 import sqlite3
 
 from user import user_table
+from flowers import flowers_table
 
 
 if __name__ == '__main__':
     connection = sqlite3.connect('tg_bot.db')
     user_table(connection)
+    flowers_table(connection)
     connection.close()
