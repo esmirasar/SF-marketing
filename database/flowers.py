6df2ff1f5b66edefde012c0e7042c7057eafd6dd
Esmira <esmirasar@yandex.ru>
2024-10-06 15:08:35 +0400

    add field

--- database/flowers.py
+++ database/flowers.py
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS flowers(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     user_id INTEGER NOT NULL,
+    schedule INTEGER NOT NULL,
     FOREIGN KEY (user_id)
 REFERENCES users(id)
 )
