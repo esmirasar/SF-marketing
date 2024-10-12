b8447a25ebae5025f475d4fce6e6f3337b4a63d2
Esmira <esmirasar@yandex.ru>
2024-10-12 12:04:40 +0400

    flowers update

--- bot/flowers.py
+++ bot/flowers.py
@@ -1,9 +1,9 @@
 import os
-import asyncio
 import aiogram
+import sqlite3
 
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, types
 from aiogram.filters.command import Command, CommandObject
 
 load_dotenv()
@@ -15,8 +15,24 @@ router = aiogram.Router()
 
 @router.message(Command("flowers"))
 async def cmd_flowers(message: types.Message, command: CommandObject) -> None:
-    name, schedule = command.args.split()
+    args = command.args.split()
+    if len(args) == 2:
+        name, schedule = command.args.split()
 
-    await message.answer(f'Введите цветок')
+        connection = sqlite3.connect('../database/tg_bot.db')
+        cursor = connection.cursor()
+        cursor.execute(f'SELECT id FROM users WHERE telegram_id={message.from_user.id}')
+        user_id = cursor.fetchone()[0]
+        try:
+            cursor.execute(f'INSERT INTO flowers (name, user_id, schedule) VALUES ("{name}", {user_id}, {schedule})')
+
+            connection.commit()
+            connection.close()
+
+            await message.answer(f'Цветок сохранен')
+        except Exception as e:
+            await message.answer(f'Кажется Вы ввели некорректные данные!')
+    else:
+        await message.answer(f'Кажется Вы ввели некорректные данные!')
 
 
