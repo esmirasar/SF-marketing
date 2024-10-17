bc35b930a4c642a7594fc5ee55648725d5da3180
Andrey <v.andrei.dmitrievich@gmail.com>
2024-10-17 15:53:18 +0300

    added timezone + menu

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types, utils
 from aiogram.fsm.storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
@@ -15,12 +15,24 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 
+async def set_bot_commands(tg_bot: Bot):
+    commands = [
+        types.BotCommand(command='/start', description='Начать работу с ботом')
+    ]
+    await tg_bot.set_my_commands(commands)
+
+
+async def on_startup(dispatcher: Dispatcher):
+    await set_bot_commands(bot)
+
+
 async def main():
     import user
     import flowers
 
     dp.include_router(user.router)
     dp.include_router(flowers.router)
+    await on_startup(dp)
     await dp.start_polling(bot)
 
 
--- bot/user.py
+++ bot/user.py
@@ -1,13 +1,15 @@
 import os
 import aiogram
+from datetime import datetime, timezone, timedelta
 
 from dotenv import load_dotenv
 
 from aiogram import Bot, types
 from aiogram.filters import command
+from aiogram.fsm.context import FSMContext
 
 from database import connection as con
-from inline_keyboards import get_menu_buttons, get_profile_buttons
+from inline_keyboards import get_menu_buttons, get_profile_buttons, get_timezone_buttons
 from config import config
 
 
@@ -21,27 +23,19 @@ router = aiogram.Router()
 
 @router.message(command.Command('start'))
 async def command_start_handler(message: types.Message) -> None:
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
 
     connection = con.connect_to_postgres()
     cursor = connection.cursor()
+    cursor.execute(f'SELECT telegram_id FROM users WHERE telegram_id={message.from_user.id}')
 
-    cursor.execute(f'SELECT telegram_id FROM users WHERE telegram_id={user_id}')
-    if not cursor.fetchone():
-        insert_query = 'INSERT INTO users (telegram_id, first_name) VALUES (%s, %s);'
-        insert_data = (user_id, user_name)
-
-        cursor.execute(insert_query, insert_data)
-
-        connection.commit()
+    if cursor.fetchone():
         connection.close()
-
-    markup = get_menu_buttons()
-
-    text = 'Вы находитесь в главном меню бота - CheFlowers!'
-    await message.delete()
-    await message.answer(text, reply_markup=markup)
+        cursor.close()
+        text = 'Вы находитесь в главном меню бота - CheFlowers!'
+        await message.answer(text=text, reply_markup=get_menu_buttons())
+    else:
+        text = 'Введите ваш часовой пояс в формате UTC±HH:MM. Например UTC+3:00.'
+        await message.answer(text=text, reply_markup=get_timezone_buttons())
 
 
 @router.callback_query(lambda call: call.data == 'profile')
@@ -62,8 +56,33 @@ async def profile_list(callback_query: types.CallbackQuery):
     cursor.close()
     connection.close()
 
-    inline_for = f'\n{"-" * 10}\n'.join([f'Название: {data[0]}\nГрафик: Полив {data[1]} раз в неделю' for data in flowers])
     if flowers:
+        inline_for = f'\n{"-" * 10}\n'.join([f'Название: {data[0]}\nГрафик: Полив каждый {data[1]}-й день' for data in flowers])
         await callback_query.message.answer(text=f'Список ваших цветов: \n{"-" * 10}\n{inline_for}', reply_markup=markup)
     else:
-        await callback_query.message.answer(text='В вашем профиле цветов нет', reply_markup=markup)
+        await callback_query.message.answer(text='В вашем профиле растений нет!', reply_markup=markup)
+
+
+@router.callback_query(lambda call: call.data[1:].isdigit() if call.data.startswith('-') else call.data.isdigit())
+async def utc_function(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    user_name = callback_query.from_user.first_name
+
+    connection = con.connect_to_postgres()
+    cursor = connection.cursor()
+
+    cursor.execute(f'SELECT telegram_id FROM users WHERE telegram_id={user_id}')
+    if not cursor.fetchone():
+        insert_query = 'INSERT INTO users (telegram_id, first_name, time_zone) VALUES (%s, %s, %s);'
+        insert_data = (user_id, user_name, callback_query.data)
+
+        cursor.execute(insert_query, insert_data)
+
+        connection.commit()
+        connection.close()
+
+    markup = get_menu_buttons()
+
+    text = 'Вы находитесь в главном меню бота - CheFlowers!'
+    await callback_query.message.delete()
+    await callback_query.message.answer(text, reply_markup=markup)
